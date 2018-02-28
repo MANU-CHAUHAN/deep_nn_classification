d31c3db32ad0a93c5d50fac20a8e8208bc54fd18
Manu Chauhan <manuchauhan1992@gmail.com>
2018-02-28 11:34:06 +0530

    run nn model for train + test

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
+import pickle
 import numpy as np
 from . import utilities
+from matplotlib import pyplot as plt
 
 """
 Main file for deep neural network classification task.
 Holds all the required functions for the classification task.
 Some functions in utilities.py as well
 """
+
+hidden_layers = [200, 100, 50]  # hidden layers only, ie without input and output layer sizes
 
 
 def initialize_parameters(layers_dims):
@@ -96,7 +101,7 @@ def model_forward(X, parameters):
 
     # Implement [LINEAR -> RELU] * (L-1) times.
     # Add "cache" to the "caches" list. Caches will be used during back propagation
-    for layer in range(1, L - 1):
+    for layer in range(1, L):
         W = parameters['W' + str(layer)]  # W for current 'layer'
         b = parameters['b' + str(layer)]  # b for current 'layer'
         A, cache = linear_activation_forward(A_prev=A, W=W, b=b, activation='relu')
@@ -131,6 +136,7 @@ def compute_cost(A_last, Y, parameters, lambd):
     # np.squeeze() is used to convert a value such as [[100]] to 100
     cost = np.squeeze(cost)
 
+    # Use regularized cost if lambda for L2 is > 0.0
     if lambd > 0.0:
         L = len(parameters) // 2
         regularization_cost = (lambd / 2 * m) * np.sum(
@@ -143,7 +149,7 @@ def linear_backward(dZ, cache, lambd):
     """
     Implement linear part for backward propagation for a single layer
     :param dZ: The gradient of cost wrt. Z for current layer
-    :param cache: dictionary containing {'A_prev_cache': A_prev, 'W_cache': W, 'b_cache': b, 'Z_cache': Z}
+    :param cache: dictionary containing 'A_prev_cache', 'W_cache', 'b_cache' and 'Z_cache'
                   for current layer, computed during forward propagation
     :param lambd: the L2 regularization lambda value
     :return: dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
@@ -151,11 +157,14 @@ def linear_backward(dZ, cache, lambd):
              db -- Gradient of the cost with respect to b (for current layer l), same shape as b
     """
 
-    A_prev, W, b = cache['A_prev_cache'], cache['W_cache'], cache['b_cache']
+    A_prev, W, b, Z = cache['A_prev_cache'], cache['W_cache'], cache['b_cache'], cache['Z_cache']
 
     m = A_prev.shape[1]
 
-    dW = 1 / m * np.dot(dZ, A_prev.T) + (lambd / m) * W
+    dW = 1 / m * np.dot(dZ, A_prev.T)
+    if lambd > 0.0:
+        dW = dW + (lambd / m) * W
+
     db = 1 / m * np.sum(dZ, axis=1, keepdims=True)
     dA_prev = np.dot(W.T, dZ)
 
@@ -180,15 +189,170 @@ def linear_activation_backward(dA, cache, activation, lambd):
             dW -- Gradient of the cost with respect to W (current layer l), same shape as W
             db -- Gradient of the cost with respect to b (current layer l), same shape as b
     """
-
-    dZ = None
     Z = cache['Z_cache']
+    dZ = None
 
     if activation == 'sigmoid':
-        dZ = utilities.sigmoid_backward(dA, cache=Z)
+        dZ = utilities.sigmoid_backward(dA=dA, cache=Z)
     elif activation == 'relu':
-        dZ = utilities.relu_backward(dA, cache=Z)
+        dZ = utilities.relu_backward(dA=dA, cache=Z)
 
     dA_prev, dW, db = linear_backward(dZ, cache, lambd)
 
     return dA_prev, dW, db
+
+
+def model_backward(AL, Y, caches, lambd):
+    """
+    Implement the backward propagation for the [LINEAR->RELU] * (L-1) times -> LINEAR -> SIGMOID group
+
+    Arguments:
+    AL -- probability vector, output of the forward propagation (L_model_forward())
+    Y -- true "label" vector (containing 0 if false else 1 if true)
+    lambd -- the regularization parameter, range: 0.0 - 1.0
+    caches -- list of caches containing:
+                every cache of linear_activation_forward() with "relu" (it's caches[l], for l in range(L-1) i.e l = 0...L-2)
+                the cache of linear_activation_forward() with "sigmoid" (it's caches[L-1])
+
+    Returns:
+    grads -- A dictionary with the gradients
+    """
+    grads = {}
+
+    Y = Y.reshape(AL.shape)
+
+    L = len(caches)
+
+    # last sigmoid layer dAL is given by -Y/A - (1 - Y)/(1 - A)
+    dAL = - np.divide(Y, AL) - np.divide((1 - Y), (1 - AL))
+
+    last_layer_cache = caches[L - 1]
+
+    # last layer grads
+    dA_prev, dW, db = linear_activation_backward(dA=dAL, cache=last_layer_cache, activation='sigmoid', lambd=lambd)
+    grads['dA' + str(L)] = dA_prev
+    grads['dW' + str(L)] = dW
+    grads['db' + str(L)] = db
+
+    # grads for other L - 1 layers
+    for layer in reversed(range(1, L)):
+        current_layer_cache = caches[layer - 1]  # caches is a list -> 0 indexed
+
+        dA_prev, dW, db = linear_activation_backward(dA=grads['dA' + str(layer + 1)], cache=current_layer_cache,
+                                                     activation='relu', lambd=lambd)
+        grads['dA' + str(layer)] = dA_prev
+        grads['dW' + str(layer)] = dW
+        grads['db' + str(layer)] = db
+
+    return grads
+
+
+def update_parameters(parameters, grads, learning_rate):
+    """
+    Updates the parameters using the calculated grads and given learning rate
+    :param parameters: the dictionary of all parameters
+    :param grads: the dictionary of grads calculated during back propagation
+    :param learning_rate: learning rate for gradient descent
+    :return: updated dictionary of parameters
+    """
+
+    L = len(parameters) // 2  # number of layers
+
+    for layer in range(1, L + 1):
+        parameters['W' + str(layer)] = parameters['W' + str(layer)] - learning_rate * grads['dW' + str(layer)]
+        parameters['b' + str(layer)] = parameters['b' + str(layer)] - learning_rate * grads['db' + str(layer)]
+
+    return parameters
+
+
+def neural_network_model(X, Y, layers_dims, iterations=1000, learning_rate=0.001, lambd=0.0):
+    """
+    Complete model connecting other parts
+    :param X: Input data
+    :param Y: Truth labels
+    :param layers_dims: the list containing the layer sizes including the input layer (1st element in layers_dims)
+    :param iterations: Number of iterations to run for gradient descent, default 1000
+    :param learning_rate: the learning rate to be used during gradient descent, default 0.001
+    :return: dictionary of updated parameters
+    """
+    parameters = initialize_parameters(layers_dims=layers_dims)
+
+    costs = []
+    iteration_numbers = []
+
+    for i in range(iterations):
+        A_last, caches = model_forward(X=X, parameters=parameters)
+
+        if i % 100 == 0:
+            cost = compute_cost(A_last=A_last, Y=Y, parameters=parameters, lambd=lambd)
+            costs.append(cost)
+            iteration_numbers.append(i)
+            print('Cost after %d iterations is %f' % (i, cost))
+
+        grads = model_backward(AL=A_last, Y=Y, caches=caches, lambd=lambd)
+
+        parameters = update_parameters(parameters=parameters, grads=grads, learning_rate=learning_rate)
+
+    return parameters, iteration_numbers, costs
+
+
+def predict(X, parameters):
+    """
+    Predict on data provided using trained parameters
+    :param X: The data for which to predict the labels
+    :param parameters: learned parameters
+    :return: array of predictions
+    """
+
+    A_last, caches = model_forward(X, parameters)
+    result = np.zeros(shape=(1, X.shape[1]))
+    for i in range(A_last.shape[1]):
+        result[0][i] = 1 if A_last[0][i] > 0.5 else 0
+    return result
+
+
+def run_nn_model(args):
+    """ Run the neural network model after collecting the data and user given inputs """
+
+    if len(args) == 1 or args[1] not in ['train', 'test', 'predict']:
+        raise Exception('\n Missing argument or Wrong argument passed')
+    else:
+        if args[1].lower() == 'train':
+            train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes = utilities.load_cat_dataset()
+
+            # reshape train/test data set from (sample_numbers, height, width, 3) to (height*width*3, sample_numbers)
+            #  3 dimension corresponds to RGB color channel values
+            train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+            train_x = train_set_x_flatten / 255  # normalize inputs: dividing by max value for a pixel
+
+            layers_dimensions = [train_x.shape[0]] + globals()['hidden_layers'] + [1]
+
+            trained_parameters, iterations, costs = neural_network_model(X=train_x, Y=train_set_y_orig,
+                                                                         layers_dims=layers_dimensions, iterations=100,
+                                                                         learning_rate=0.001, lambd=0.7)
+            with open('trained_parameters.pkl', 'wb') as file:
+                pickle.dump(trained_parameters, file)
+
+            plt.plot(iterations, costs)
+            plt.xlabel('Number of Iterations')
+            plt.ylabel('Training Cost')
+            plt.show()
+
+        elif args[1].lower() == 'test':
+            if not os.path.isfile('trained_parameters.pkl'):
+                raise Exception('\n Trained parameter pickle file is not present.')
+
+            train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes = utilities.load_cat_dataset()
+
+            # reshape train/test data set from (sample_numbers, height, width, 3) to (height*width*3, sample_numbers)
+            #  3 dimension corresponds to RGB color channel values
+            test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+            test_set_x = test_set_x_flatten / 255  # normalize inputs: dividing by max value for a pixel
+
+            with open('trained_parameters.pkl', 'rb') as file:
+                trained_parameters = pickle.load(file)
+
+            predictions = predict(X=test_set_x, parameters=trained_parameters)
+
+            print('\n \n Accuracy for test set data: %f' % np.mean(predictions == test_set_y_orig) * 100)
+            
